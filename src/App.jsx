688efeafc9e05f20096eee637ccd1fec4ec065a2
Ibraheem <iejibode@gmail.com>
2023-03-28 05:31:41 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import { useState } from 'react';
 import { Job } from './components/Job';
 import './App.css';
-import { Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Filter from './components/Filter';
 
 function App() {
   const [job, setJob] = useState([]);
   const [currentItem, setCurrentItem] = useState(0);
   return (
     <>
-      <Filter currentItem={currentItem} setCurrentItem={setCurrentItem} />
-      <Job job={job} currentItem={currentItem} />
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
     </>
   );
 }
